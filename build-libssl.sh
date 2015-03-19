28fc129038b752e4e25d46b3ee020e0a883afd77
Joe Neuman <joe@icracked.com>
2015-03-19 10:57:37 +0100

    Run building in parallel

--- build-libssl.sh
+++ build-libssl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#  Automatic build script for libssl and libcrypto 
+#  Automatic build script for libssl and libcrypto
 #  for iPhoneOS and iPhoneSimulator
 #
 #  Created by Felix Schulze on 16.12.10.
@@ -20,7 +20,7 @@
 #
 ###########################################################################
 #  Change values here													  #
-#				
+#
 VERSION="1.0.2"													      #
 SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`														  #
 #																		  #
@@ -44,14 +44,14 @@ if [ ! -d "$DEVELOPER" ]; then
   exit 1
 fi
 
-case $DEVELOPER in  
+case $DEVELOPER in
      *\ * )
            echo "Your Xcode path contains whitespaces, which is not supported."
            exit 1
           ;;
 esac
 
-case $CURRENTPATH in  
+case $CURRENTPATH in
      *\ * )
            echo "Your path contains whitespaces, which is not supported by 'make install'."
            exit 1
@@ -73,17 +73,16 @@ mkdir -p "${CURRENTPATH}/lib"
 tar zxf openssl-${VERSION}.tar.gz -C "${CURRENTPATH}/src"
 cd "${CURRENTPATH}/src/openssl-${VERSION}"
 
-
-for ARCH in ${ARCHS}
-do
-	if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
+buildArch () {
+  ARCH = $1
+  if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
 	then
 		PLATFORM="iPhoneSimulator"
 	else
 		sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
 		PLATFORM="iPhoneOS"
 	fi
-	
+
 	export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
 	export CROSS_SDK="${PLATFORM}${SDKVERSION}.sdk"
 	export BUILD_TOOLS="${DEVELOPER}"
@@ -103,9 +102,9 @@ do
     else
 	    ./Configure iphoneos-cross --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
     fi
-    
+
     if [ $? != 0 ];
-    then 
+    then
     	echo "Problem while configure - Please check ${LOG}"
     	exit 1
     fi
@@ -119,17 +118,77 @@ do
 	else
 		make >> "${LOG}" 2>&1
 	fi
-	
+
 	if [ $? != 0 ];
-    then 
+    then
     	echo "Problem while make - Please check ${LOG}"
     	exit 1
     fi
-    
+
     set -e
 	make install >> "${LOG}" 2>&1
 	make clean >> "${LOG}" 2>&1
+}
+
+for ARCH in ${ARCHS}
+do
+  {
+    if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
+  	then
+  		PLATFORM="iPhoneSimulator"
+  	else
+  		sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
+  		PLATFORM="iPhoneOS"
+  	fi
+
+  	export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
+  	export CROSS_SDK="${PLATFORM}${SDKVERSION}.sdk"
+  	export BUILD_TOOLS="${DEVELOPER}"
+
+  	echo "Building openssl-${VERSION} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
+  	echo "Please stand by..."
+
+  	export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH}"
+  	mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
+  	LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-openssl-${VERSION}.log"
+
+  	set +e
+      if [[ "$VERSION" =~ 1.0.0. ]]; then
+  	    ./Configure BSD-generic32 --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
+  	elif [ "${ARCH}" == "x86_64" ]; then
+  	    ./Configure darwin64-x86_64-cc --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
+      else
+  	    ./Configure iphoneos-cross --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
+      fi
+
+      if [ $? != 0 ];
+      then
+      	echo "Problem while configure - Please check ${LOG}"
+      	exit 1
+      fi
+
+  	# add -isysroot to CC=
+  	sed -ie "s!^CFLAG=!CFLAG=-isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -miphoneos-version-min=7.0 !" "Makefile"
+
+  	if [ "$1" == "verbose" ];
+  	then
+  		make
+  	else
+  		make >> "${LOG}" 2>&1
+  	fi
+
+  	if [ $? != 0 ];
+      then
+      	echo "Problem while make - Please check ${LOG}"
+      	exit 1
+      fi
+
+      set -e
+  	make install >> "${LOG}" 2>&1
+  	make clean >> "${LOG}" 2>&1
+  } &
 done
+wait
 
 echo "Build library..."
 lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/libssl.a ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-x86_64.sdk/lib/libssl.a  ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/libssl.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7s.sdk/lib/libssl.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-arm64.sdk/lib/libssl.a -output ${CURRENTPATH}/lib/libssl.a
